c0474ec99c5c618079116e74ea72aa0583267067
zsline <sergei.dereka@gmail.com>
2025-07-21 15:21:53 +0300

    calendar-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,30 +275,49 @@ if(accordion.length > 0){
 }
 
 new AirDatepicker('#start-date', {
-  dateFormat: 'E dd MMM'
+  locale: {
+        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+  },
+  autoClose:true,
+  dateFormat: 'E dd MMM',
+  isMobile: true
 });
 new AirDatepicker('#start-time', {
   timepicker:true,
-  onlyTimepicker: true
+  onlyTimepicker: true,
+  isMobile: true
+});
+new AirDatepicker('#end-date', {
+  locale: {
+        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+  },
+  autoClose:true,
+  dateFormat: 'E dd MMM',
+  isMobile: true
+});
+new AirDatepicker('#end-time', {
+  timepicker:true,
+  onlyTimepicker: true,
+  isMobile: true
 });
 
-  flatpickr("#start-date", {
-    dateFormat: 'D d M',
-  });
-  flatpickr("#end-date", {
-    dateFormat: 'D d M',
-  });
-  flatpickr("#start-time", {
-    enableTime: true,
-    noCalendar: true,
-    time_24hr: true,
-    dateFormat: 'H:i',
-  });
-  flatpickr("#end-time", {
-    enableTime: true,
-    noCalendar: true,
-    time_24hr: true,
-    dateFormat: 'H:i',
-  });
 });
 
